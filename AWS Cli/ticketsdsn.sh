a676a757627ff25b2f98dfc806d5f9fc054652f5
mgarciah07 <mgarciah07@educantabria.es>
2025-01-31 00:34:20 +0100

    Añadidos comentarios al script dns

--- AWS Cli/ticketsdsn.sh
+++ AWS Cli/ticketsdsn.sh
@@ -3,6 +3,7 @@
 mkdir duckdns
 cd duckdns
 
+# Poner entre las comillas el echo url=...
 echo 'echo url="https://www.duckdns.org/update?domains=marcosticket&token=899e823b-dbdf-4fda-ba95-5138dc4939b7&ip=" | curl -k -o ~/duckdns/duck.log -K -'>duck.sh
 chmod 700 duck.sh
 
