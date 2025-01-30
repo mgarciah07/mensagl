9851f4d0137df8e7dcebbbfd657b733d6b43249c
mgarciah07 <mgarciah07@educantabria.es>
2025-01-30 13:38:40 +0100

    creación de subredes para RDS añadido

--- AWS Cli/instancias.sh
+++ AWS Cli/instancias.sh
@@ -4,6 +4,8 @@
 key_name="ssh-mensagl-2025-marcos"
 ami_id="ami-04b4f1a9cf54c11d0"  # Reemplaza con el ID de la AMI de Ubuntu que desees usar
 instance_type="t2.micro"
+region="us-east-1"
+bucket_name="copias-seguridad-unique-name-$(date +%s)"  # Usa un nombre único
 
 # Crear un par de claves
 aws ec2 create-key-pair --key-name "$key_name" --query 'KeyMaterial' --output text > "${key_name}.pem"
@@ -15,6 +17,14 @@ subnet_public1_id=$(aws ec2 describe-subnets --filters "Name=tag:Name,Values=vpc
 subnet_public2_id=$(aws ec2 describe-subnets --filters "Name=tag:Name,Values=vpc-mensagl-2025-Marcos-subnet-public2-us-east-1b" --query "Subnets[0].SubnetId" --output text)
 subnet_private1_id=$(aws ec2 describe-subnets --filters "Name=tag:Name,Values=vpc-mensagl-2025-Marcos-subnet-private1-us-east-1a" --query "Subnets[0].SubnetId" --output text)
 subnet_private2_id=$(aws ec2 describe-subnets --filters "Name=tag:Name,Values=vpc-mensagl-2025-Marcos-subnet-private2-us-east-1b" --query "Subnets[0].SubnetId" --output text)
+
+# Define el nombre del grupo de subredes y la descripción
+rds_subnet_group_name="rds-subnet-group-mensagl-2025"
+description="Subnet group for RDS instances"
+subnet_ids="$subnet_private1_id $subnet_private2_id"  # Reemplaza con los IDs de tus subredes privadas
+
+# Crear el grupo de subredes RDS
+aws rds create-db-subnet-group --db-subnet-group-name "$rds_subnet_group_name" --db-subnet-group-description "$description" --subnet-ids $subnet_ids
 
 # Crear grupos de seguridad
 sg_haproxy_id=$(aws ec2 create-security-group --group-name "sg_HAProxy" --description "Security group for HAProxy" --vpc-id "$vpc_id" --query 'GroupId' --output text)
@@ -47,10 +57,18 @@ aws ec2 run-instances --image-id "$ami_id" --count 1 --instance-type "$instance_
 aws ec2 run-instances --image-id "$ami_id" --count 1 --instance-type "$instance_type" --key-name "$key_name" --security-group-ids "$sg_wordpress_id" --subnet-id "$subnet_private2_id" --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Wordpress2}]'
 
 # Crear RDS PostgreSQL para Matrix-Synapse
-aws rds create-db-instance --db-instance-identifier postgres-Matrix --db-instance-class db.t3.micro --engine postgres --master-username admin --master-user-password password --allocated-storage 20 --vpc-security-group-ids "$sg_matrix_synapse_id"
+aws rds create-db-instance --db-instance-identifier postgres-Matrix --db-instance-class db.t3.micro --engine postgres --master-username admin --master-user-password password --allocated-storage 20 --vpc-security-group-ids "$sg_matrix_synapse_id" --db-subnet-group-name "$subnet_private1_id"
 
 # Crear RDS MySQL para Wordpress
-aws rds create-db-instance --db-instance-identifier mysql-Wordpress --db-instance-class db.t3.micro --engine mysql --master-username admin --master-user-password password --allocated-storage 20 --vpc-security-group-ids "$sg_wordpress_id"
+aws rds create-db-instance --db-instance-identifier mysql-Wordpress --db-instance-class db.t3.micro --engine mysql --master-username admin --master-user-password password --allocated-storage 20 --vpc-security-group-ids "$sg_wordpress_id" --db-subnet-group-name "$subnet_private1_id"
 
 # Crear bucket S3 para copias de seguridad
-aws s3api create-bucket --bucket copiasSeguridad --region us-east-1
+aws s3api create-bucket --bucket "$bucket_name" --region "$region"
+
+# Cambios realizados:
+
+#     Nombre único para el bucket S3: Usa copias-seguridad-unique-name-$(date +%s) para asegurar que el nombre sea único.
+
+#     Subred especificada para RDS: He añadido --db-subnet-group-name para las instancias RDS, reemplazando "$subnet_private1_id" con un nombre de grupo de subredes válido. Asegúrate de crear un grupo de subredes de RDS primero si aún no lo has hecho.
+
+# Si necesitas más detalles o ayuda con algún paso en particular, ¡estaré encantado de ayudarte!
