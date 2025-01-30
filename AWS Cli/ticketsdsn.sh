1183026de388f9b6eb04ab80cbb52ebb847cd7ba
mgarciah07 <mgarciah07@educantabria.es>
2025-01-31 00:20:59 +0100

    Duck dns automático

---
+++ AWS Cli/ticketsdsn.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir duckdns
+cd duckdns
+
+echo 'echo url="https://www.duckdns.org/update?domains=marcosticket&token=899e823b-dbdf-4fda-ba95-5138dc4939b7&ip=" | curl -k -o ~/duckdns/duck.log -K -'>duck.sh
+chmod 700 duck.sh
+
+
+tarea="*/5 * * * * ~/duckdns/duck.sh >/dev/null 2>&1"
+# Añade la tarea cron al crontab actual
+(crontab -l 2>/dev/null; echo "$tarea") | crontab -
+
+./duck.sh
