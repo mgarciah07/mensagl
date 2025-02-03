6376c37fe8c37f0ace53496f4063d66a5965fdbc
mgarciah07 <mgarciah07@educantabria.es>
2025-02-03 13:49:16 +0100

    Asignar IP fija a las instancias

--- aws-cli/instancias semana1.sh
+++ aws-cli/instancias semana1.sh
@@ -35,8 +35,14 @@ aws ec2 authorize-security-group-ingress --group-id "$sg_wordpress_id" --protoco
 aws ec2 authorize-security-group-ingress --group-id "$sg_wordpress_id" --protocol tcp --port 80 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-id "$sg_wordpress_id" --protocol tcp --port 443 --cidr 0.0.0.0/0
 
+# Crear interfaces de red con IPs privadas específicas y asociarlas con las instancias
+interface_matrix_id=$(aws ec2 create-network-interface --subnet-id "$subnet_public1_id" --private-ip-address "10.210.1.2" --description "Interface for Matrix-Synapse" --query 'NetworkInterface.NetworkInterfaceId' --output text)
+interface_wordpress_id=$(aws ec2 create-network-interface --subnet-id "$subnet_public2_id" --private-ip-address "10.210.2.2" --description "Interface for Wordpress" --query 'NetworkInterface.NetworkInterfaceId' --output text)
+
 # Crear instancias Matrix-Synapse en subredes publicas con IP pública
-aws ec2 run-instances --image-id "$ami_idmatrix" --count 1 --instance-type "$instance_type" --key-name "$key_name" --security-group-ids "$sg_matrix_synapse_id" --subnet-id "$subnet_public1_id" --associate-public-ip-address --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Matrix-Synapse1}]' --user-data file://../EC2-User/matrixdns.sh
+instance_matrix_id=$(aws ec2 run-instances --image-id "$ami_idmatrix" --count 1 --instance-type "$instance_type" --key-name "$key_name" --security-group-ids "$sg_matrix_synapse_id" --network-interface "NetworkInterfaceId=$interface_matrix_id,DeviceIndex=0,AssociatePublicIpAddress=true" --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Matrix-Synapse1}]' --query 'Instances[0].InstanceId' --output text --user-data file://../EC2-User/matrixdns.sh)
 
 # Crear instancias Wordpress en subredes publicas con IP pública
-aws ec2 run-instances --image-id "$ami_idticket" --count 1 --instance-type "$instance_type" --key-name "$key_name" --security-group-ids "$sg_wordpress_id" --subnet-id "$subnet_public2_id" --associate-public-ip-address --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Wordpress1}]' --user-data file://../EC2-User/ticketsdns.sh
+instance_wordpress_id=$(aws ec2 run-instances --image-id "$ami_idticket" --count 1 --instance-type "$instance_type" --key-name "$key_name" --security-group-ids "$sg_wordpress_id" --network-interface "NetworkInterfaceId=$interface_wordpress_id,DeviceIndex=0,AssociatePublicIpAddress=true" --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Wordpress1}]' --query 'Instances[0].InstanceId' --output text --user-data file://../EC2-User/ticketsdns.sh)
+
+echo "Instancias creadas y configuradas con IPs privadas específicas y IP pública."
