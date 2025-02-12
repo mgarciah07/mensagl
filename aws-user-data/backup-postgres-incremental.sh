40da1f64bc1bce32713f6732b2b827b31bcc6320
mgarciah07 <mgarciah07@educantabria.es>
2025-02-12 10:05:23 +0100

    cambios

--- aws-user-data/backup-postgres-incremental.sh
+++ aws-user-data/backup-postgres-incremental.sh
@@ -17,37 +17,37 @@ apt install -y rsync
 apt install -y awscli
 
 # Crea el archivo de script de respaldo
-cat <<EOF > /home/postgres/backup-postgres.sh
+cat <<EOF > /home/ubuntu/backup-postgres.sh
 
 #!/bin/bash
 
 # Variables
-BACKUP_DIR="/home/postgres/backups"
+BACKUP_DIR="/home/ubuntu/backups"
 INCREMENTAL_DIR="/home/postgres/backups-incremental"
-DATE=\$(date +%Y-%m-%d)
-S3_BUCKET="s3://tu-bucket-de-s3"
+DATE=$(date +%Y-%m-%d)
+S3_BUCKET="s3://s3-mensagl-marcos"
 
 # Crear directorio de backups incrementales si no existe
-mkdir -p "\${INCREMENTAL_DIR}"
+mkdir -p "${INCREMENTAL_DIR}"
 
 # Sincronizar cambios incrementales al directorio incremental
-rsync -av --delete "\${BACKUP_DIR}/" "\${INCREMENTAL_DIR}/" || { echo "Error en rsync"; exit 1; }
+rsync -av --delete "${BACKUP_DIR}/" "${INCREMENTAL_DIR}/" || { echo "Error en rsync"; exit 1; }
 
 # Comprimir el directorio incremental
-tar -czf "\${INCREMENTAL_DIR}/backup_\${DATE}.tar.gz" -C "\${INCREMENTAL_DIR}" . || { echo "Error al comprimir"; exit 1; }
+tar -czf "${INCREMENTAL_DIR}/backup_\${DATE}.tar.gz" -C "${INCREMENTAL_DIR}" . || { echo "Error al comprimir"; exit 1; }
 
 # Transferir la copia de seguridad a S3
-aws s3 cp "\${INCREMENTAL_DIR}/backup_\${DATE}.tar.gz" "\${S3_BUCKET}/backup_\${DATE}.tar.gz" || { echo "Error al subir a S3"; exit 1; }
+aws s3 cp "${INCREMENTAL_DIR}/backup_\${DATE}.tar.gz" "${S3_BUCKET}/backup_\${DATE}.tar.gz" || { echo "Error al subir a S3"; exit 1; }
 
 EOF
 
 # Asegura que el archivo de backup sea ejecutable
-chmod 700 /home/postgres/backup-postgres.sh
+chmod 700 /home/ubuntu/backup-postgres.sh
 
 # Configura la tarea cron para ejecutar el backup a las 2:00 AM todos los días
-tarea="0 2 * * * /home/postgres/backup-postgres.sh >/dev/null 2>&1"
+tarea="0 2 * * * /home/ubuntu/backup-postgres.sh >/dev/null 2>&1"
 
 # Añade la tarea cron al crontab actual, sin duplicar
-(crontab -l 2>/dev/null; echo "\$tarea") | crontab -
+(crontab -l 2>/dev/null; echo "$tarea") | crontab -
 
 fi
